8eadfa479c221f71c4bab7cf701b929921a70a1c
Pradhish87 <pradhishm87@gmail.com>
2026-01-15 19:18:06 +0530

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,19 @@
+
+
+name = 'Pradhish M'  #NAME
+role = 'python DEVELOPER' #ROLE
+date = "15-01-2026"  #DATE
+
+print('NAME : ',name)
+print('ROLE : ',role)
+print('DATE : ',date)
+
+print()
+
+#user_input
+name = input('Enter your name: ') #input of username
+role = input('Enter your role: ') #input of user role
+date = input('Enter your date: ') #input of user date
+print('Your name is ',name)
+print('Your role is ',role)
+print('Your date is ',date)
